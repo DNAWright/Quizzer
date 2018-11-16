d14643d70bd1f72ef8658842017b8dcffb1bdd50
DNAWright <dfwrite@gmail.com>
2018-11-16 00:28:19 -0500

    Forgot to add finished product to repo

--- src/QuizDisplay.java
+++ src/QuizDisplay.java
@@ -1,7 +1,9 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Time;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 import javax.swing.*;
 
 
@@ -26,70 +28,115 @@ public class QuizDisplay extends JFrame
                     "tyrosine", "valine"
             };
 
-    Thread timer = new Thread(new Runnable() {
-        public void run()
-        {
-            if(System.currentTimeMillis() >= timeRemaining)
+    // New runnable to check remaining time and report it to user in real time
+    public class timeClock implements Runnable{
+            public void run()
             {
-                
-            }
-        }});
 
-    public long timeRemaining = 0;
+                long endTime = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis()) + 30;
+
+                while(TimeUnit.MILLISECONDS.toSeconds((System.currentTimeMillis())) <= endTime)
+                {
+                    if(endTime <= TimeUnit.MILLISECONDS.toSeconds((System.currentTimeMillis())) )
+                    {
+                        answerText.setEditable(false);
+                        statusText.setText("Time up!");
+                        timeRemaining = 0;
+                        timeRemainingField.setText("0");
+                    }
+                    else
+                    {
+                        try
+                        {
+                            timeRemaining = endTime - TimeUnit.MILLISECONDS.toSeconds((System.currentTimeMillis()));
+                            timeRemainingField.setText(String.valueOf(timeRemaining));
+                            Thread.sleep(1000);
+                        }
+                        catch(InterruptedException e)
+                        {
+                        }
+                    }
+
+                }
+            }
+    }
+
+    long timeRemaining = 0;
+    int indexPointer = 0;
+    int pointsCorrect =  0;
+    int pointsIncorrect = 0;
+
+    Random rand = new Random();
 
     private static final long serial = -524526543L;
-    private boolean isAboard = true;
-    private String destination = "";
-    private JLabel destinationLabel = new JLabel("Amino Acid Symbol: ");
+    private String answer = "";
+    private JLabel aminoLabel = new JLabel("Amino Acid Symbol: ");
     private JLabel consoleLabel = new JLabel("Instructions: ");
     private JLabel infoLabel = new JLabel("Right || Wrong || Time");
     private TextField answerText = new TextField();
     private TextArea statusText = new TextArea();
     private TextField totalRight = new TextField();
     private TextField totalWrong = new TextField();
-    private TextField timeRemaining = new TextField();
+    private TextField timeRemainingField = new TextField();
     private JButton startButton = new JButton("Start");
     private JButton cancelButton = new JButton("Cancel");
 
 
+    // Action Listener for the start button. Starts new timeClock thread and unlocks answer text
     private class startQuiz implements ActionListener
     {
         @Override
         public void actionPerformed(ActionEvent e)
         {
-            Random rand = new Random();
-            int indexPointer = rand.nextInt(20);
+            indexPointer = rand.nextInt(20);
 
+            pointsCorrect = 0;
+            pointsIncorrect = 0;
+            totalRight.setText("0");
+            totalWrong.setText("0");
             statusText.setText("Enter Symbol for Amino Acid: " + FULL_NAMES[indexPointer]);
+            answerText.setEditable(true);
 
-            timeRemaining = System.currentTimeMillis() + (30 * 1000);
+            timeClock tc = new timeClock();
+            new Thread(tc).start();
         }
     }
 
-    private class getDestination implements ActionListener
+    // New ActionListener that checks the user input for correct answers and updates stats accordingly
+    private class checkAnswer implements ActionListener
     {
         @Override
         public void actionPerformed(ActionEvent e)
         {
 
-            destination = answerText.getText();
-        }
-    }
+            answer = answerText.getText().toUpperCase();
 
-    private class retCrew implements ActionListener
-    {
-        @Override
-        public void actionPerformed(ActionEvent e)
-        {
-            if(isAboard == true)
+            if(answer.equals(SHORT_NAMES[indexPointer]))
             {
-                statusText.setText("The crew is already aboard and quite bored. Try sending them somewhere.");
+                pointsCorrect += 1;
+                totalRight.setText(String.valueOf(pointsCorrect));
+                statusText.setText("Correct!");
             }
             else
             {
-                statusText.setText("Bringing the crew back with haste!");
-                isAboard = true;
+                pointsIncorrect += 1;
+                totalWrong.setText(String.valueOf(pointsIncorrect));
+                statusText.setText("Incorrect...");
             }
+            indexPointer = rand.nextInt(20);
+            answerText.setText("");
+            statusText.setText("Enter Symbol for Amino Acid: " + FULL_NAMES[indexPointer]);
+        }
+    }
+
+    // Action Listener for the cancel button that stops the quiz
+    private class cancelQuiz implements ActionListener
+    {
+        @Override
+        public void actionPerformed(ActionEvent e)
+        {
+            statusText.setText("Quiz cancelled with " + timeRemaining + " seconds left.");
+            answerText.setEditable(false);
 
         }
     }
@@ -98,7 +145,7 @@ public class QuizDisplay extends JFrame
     private QuizDisplay()
     {
         super("Enterprise Control Panel");
-        setSize(500,500);
+        setSize(500,300);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -109,7 +156,7 @@ public class QuizDisplay extends JFrame
         buttonPanel.add(cancelButton);
 
         JPanel destPanel = new JPanel();
-        destPanel.add(destinationLabel);
+        destPanel.add(aminoLabel);
         destPanel.add(answerText);
 
         JPanel consPanel = new JPanel();
@@ -120,15 +167,16 @@ public class QuizDisplay extends JFrame
         infoPanel.add(infoLabel);
         infoPanel.add(totalRight);
         infoPanel.add(totalWrong);
-        infoPanel.add(timeRemaining);
+        infoPanel.add(timeRemainingField);
 
         getContentPane().add(buttonPanel, BorderLayout.SOUTH);
         getContentPane().add(destPanel, BorderLayout.CENTER);
         getContentPane().add(consPanel, BorderLayout.NORTH);
         getContentPane().add(infoPanel, BorderLayout.EAST);
         startButton.addActionListener(new startQuiz());
-        cancelButton.addActionListener(new retCrew());
-        answerText.addActionListener(new getDestination());
+        cancelButton.addActionListener(new cancelQuiz());
+        answerText.addActionListener(new checkAnswer());
+        answerText.setEditable(false);
         answerText.setColumns(2);
         totalRight.setColumns(2);
         totalRight.setText("0");
@@ -136,9 +184,9 @@ public class QuizDisplay extends JFrame
         totalWrong.setColumns(2);
         totalWrong.setText("0");
         totalWrong.setEditable(false);
-        timeRemaining.setColumns(4);
-        timeRemaining.setText(":30");
-        timeRemaining.setEditable(false);
+        timeRemainingField.setColumns(4);
+        timeRemainingField.setText("30");
+        timeRemainingField.setEditable(false);
         statusText.setText("Type in the correct Amino Acid for: ");
         statusText.setEditable(false);
 
